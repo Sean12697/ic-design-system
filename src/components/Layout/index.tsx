cbf30f8cf437849e8a86f2d79208ac48da2f601f
jd - Legend <116016939+jd239@users.noreply.github.com>
2022-11-22 22:20:08 +0000

    fix(root): cookie banner and policy integration

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { withPrefix } from "gatsby";
 
 import { Helmet } from "react-helmet";
@@ -8,6 +8,9 @@ import "./index.css";
 import TopNavWrapper from "../TopNavWrapper";
 import { GCHQLogo, MI5Logo, SISLogo } from "../../assets/svg";
 import CookieBanner from "../CookieBanner";
+
+import CookieConsentContext from "../../context/CookieConsentContext";
+import { consentCookieApproved } from "../CookieBanner/cookies.helper";
 
 const {
   STATUS,
@@ -61,58 +64,102 @@ const Layout: React.FC<LayoutProps> = ({
   title = "",
   description = "",
   children,
-}) => (
+}) => {
+  const {
+    NODE_ENV,
+    GATSBY_GA_TRACKING_ID
+  } = process.env;
+  const IN_PRODUCTION = NODE_ENV === 'production';
+
+  let defaultCookieConsentValue;
+
+  console.log(GATSBY_GA_TRACKING_ID);
+
+  // document object is not available during SSR so a check is in place.
+  // document.cookie is required for consentCookieApproved method
+  if (typeof document !== 'undefined') {
+    defaultCookieConsentValue = consentCookieApproved();
+  } else {
+    defaultCookieConsentValue = false;
+  }
+
+  const [cookieConsent, setCookieConsent] = useState(defaultCookieConsentValue);
+
+  const handleCookieConsent = (consent: boolean) => {
+    setCookieConsent(consent);
+  }
+
+  const value = React.useMemo(() => ({
+    cookieConsent, handleCookieConsent
+  }), [cookieConsent, handleCookieConsent])
+
+  return (
   <>
     <Helmet>
       <html lang="en" />
       <title>{title || TITLE} - Intelligence Community Design System</title>
       <meta name="description" content={`${description || META_DESCRIPTION}`} />
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GATSBY_GA_TRACKING_ID}`} />
+      <script>
+        {`
+            window.dataLayer = window.dataLayer || [  ];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'default', {
+              'ad_storage': '${cookieConsent && IN_PRODUCTION ? 'granted' : 'denied'}',
+              'analytics_storage': '${cookieConsent && IN_PRODUCTION ? 'granted' : 'denied'}',
+            });
+            gtag('js', new Date());
+            gtag('config', '${GATSBY_GA_TRACKING_ID}');
+        `}
+      </script>
     </Helmet>
-    <ClientOnly>
-      {title !== "Cookies Policy" && <CookieBanner />}
-      <div className="main-page-container">
-        <IcLink href="#main" id="skip" className="skip-content-link">
-          Skip to main content
-        </IcLink>
-        <TopNavWrapper appTitle={TITLE} status={STATUS} version={VERSION} />
-        <main id="main" className="homepage-wrapper">
-          {children}
-          {!contentProps?.index && <IcBackToTop target="main" />}
-        </main>
-      </div>
-      <div className="footer">
-        <IcFooter
-          description={FOOTER_PROPS.content}
-          caption={FOOTER_PROPS.caption}
-        >
-          {FOOTER_PROPS.footerLinks.map((footerLinks: FooterLinks) => (
-            <IcFooterLink
-              slot="link"
-              href={withPrefix(footerLinks.link)}
-              key={footerLinks.key}
-            >
-              {footerLinks.text}
-            </IcFooterLink>
-          ))}
-          <div slot="logo" className="logo-wrapper">
-            <IcFooterLink href="https://sis.gov.uk">
-              <SISLogo aria-labelledby="SIS Logo" />
-              <span className="link-text">Go to SIS website</span>
-            </IcFooterLink>
-            <IcFooterLink href="https://www.mi5.gov.uk">
-              <MI5Logo aria-labelledby="MI5 Logo" />
-              <span className="link-text">Go to MI5 website</span>
-            </IcFooterLink>
-            <IcFooterLink href="https://gchq.gov.uk">
-              <GCHQLogo aria-labelledby="GCHQ Logo" />
-              <span className="link-text">Go to GCHQ website</span>
-            </IcFooterLink>
-          </div>
-        </IcFooter>
-      </div>
-    </ClientOnly>
+    <CookieConsentContext.Provider value={value}>
+      <ClientOnly>
+        {title !== "Cookies Policy" && <CookieBanner />}
+        <div className="main-page-container">
+          <IcLink href="#main" id="skip" className="skip-content-link">
+            Skip to main content
+          </IcLink>
+          <TopNavWrapper appTitle={TITLE} status={STATUS} version={VERSION} />
+          <main id="main" className="homepage-wrapper">
+            {children}
+            {!contentProps?.index && <IcBackToTop target="main" />}
+          </main>
+        </div>
+        <div className="footer">
+          <IcFooter
+            description={FOOTER_PROPS.content}
+            caption={FOOTER_PROPS.caption}
+          >
+            {FOOTER_PROPS.footerLinks.map((footerLinks: FooterLinks) => (
+              <IcFooterLink
+                slot="link"
+                href={withPrefix(footerLinks.link)}
+                key={footerLinks.key}
+              >
+                {footerLinks.text}
+              </IcFooterLink>
+            ))}
+            <div slot="logo" className="logo-wrapper">
+              <IcFooterLink href="https://sis.gov.uk">
+                <SISLogo aria-labelledby="SIS Logo" />
+                <span className="link-text">Go to SIS website</span>
+              </IcFooterLink>
+              <IcFooterLink href="https://www.mi5.gov.uk">
+                <MI5Logo aria-labelledby="MI5 Logo" />
+                <span className="link-text">Go to MI5 website</span>
+              </IcFooterLink>
+              <IcFooterLink href="https://gchq.gov.uk">
+                <GCHQLogo aria-labelledby="GCHQ Logo" />
+                <span className="link-text">Go to GCHQ website</span>
+              </IcFooterLink>
+            </div>
+          </IcFooter>
+        </div>
+      </ClientOnly>
+    </CookieConsentContext.Provider>
     <RouteAnnouncer page={`${title || ""} - ${TITLE}`} />
   </>
-);
+)};
 
 export default Layout;
